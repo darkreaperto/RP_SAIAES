02433e225001b6e4a9703a632d7cf7947067ffd4
aoihanabi <abii98cm@gmail.com>
2018-10-19 09:41:48 -0600

    Clases controladores y modelos de madera, tipo de producto y tipo de madera. Interfaz: llenar tablas y combobox

--- src/logica/negocio/Madera.java
+++ src/logica/negocio/Madera.java
@@ -5,10 +5,259 @@
  */
 package logica.negocio;
 
+import util.Estado;
+
 /**
- * Instancia los clientes con sus atributos. 
+ * Instancia los productos con sus atributos. 
  * @author dark-reaper
  */
 public class Madera {
+    private String codigo;
+    private String codProducto;
+    private String nombre;
+    private String codTipoMadera;
+    private String descTipoMadera;
+    private String medidas;
+    private String codTipoProducto;
+    private String descTipoProducto;
+    private int cantidad;
+    private double precioXvara;
+    private String descripcion;
+    private Estado estado;
+    private String codProveedor;
+    
+    /**
+     * Constructor vacío de clase madera.
+     */
+    public Madera() {
+        
+    }
+    /**
+     * Constructor de clase madera, inicializa variables.
+     * @param codigo codigo producto.
+     * @param codProducto codigo especificado de producto.
+     * @param nombre nombre del producto(madera).
+     * @param codTipoMadera codigo del tipo de madera.
+     * @param descTipoMadera descripcion del tipo de madera.
+     * @param medidas medidas del producto de acuerdo a su tipo.
+     * @param codTipoProducto tipo de producto(troza, acerrada, terminada).
+     * @param cantidad unidades de producto.
+     * @param descTipoProducto descripcion del tipo de producto.
+     * @param precioXvara precio por vara del producto.
+     * @param descripcion descripciones detalladas del producto.
+     * @param estado Estado de producto.
+     * @param codProveedor codigo del proveedor.
+     */
+    public Madera(String codigo, String codProducto, String nombre, 
+            String codTipoMadera, String descTipoMadera, String medidas, 
+            String codTipoProducto, String descTipoProducto, int cantidad, 
+            double precioXvara, String descripcion, String estado,
+            String codProveedor) {
+        this.codigo = codigo;
+        this.codProducto = codProducto;
+        this.nombre = nombre;
+        this.codTipoMadera = codTipoMadera;
+        this.descTipoMadera = descTipoMadera;
+        this.medidas = medidas;
+        this.codTipoProducto = codTipoProducto;
+        this.descTipoProducto = descTipoProducto;
+        this.cantidad = cantidad;
+        this.precioXvara = precioXvara;
+        this.descripcion = descripcion;
+        this.estado = estado.equals("A") ? Estado.Activo : Estado.Deshabilitado;
+        this.codProveedor = codProveedor;
+        
+    }
+
+    /**
+     * Obtener el codigo.
+     * @return el codigo.
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+    
+    /**
+     * Establecer codigo de producto
+     * @param codigo El codigo en la BD.
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    /**
+     * Obtener el codigo personalizado del producto.
+     * @return el codigo del producto
+     */
+    public String getCodProducto() {
+        return codProducto;
+    }
+    
+    /**
+     * Establecer codigo personalizado del producto
+     * @param codProducto  El codigo especificado del producto.
+     */
+    public void setCodProducto(String codProducto) {
+        this.codProducto = codProducto;
+    }
+    
+    /**
+     * Obtener el nombre del producto.
+     * @return el nombre 
+     */
+    public String getNombre() {
+        return nombre;
+    }
+    
+    /**
+     * Establecer nombre de producto
+     * @param nombre El nombre.
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    /**
+     * Obtener el codigo del tipo de madera del producto.
+     * @return el codigo de tipo de madera
+     */
+    public String getCodTipoMadera() {
+        return codTipoMadera;
+    }
+    
+    /**
+     * Establecer codigo de tipo de madera de producto
+     * @param codTipoMadera codigo del tipo de madera
+     */
+    public void setCodTipoMadera(String codTipoMadera) {
+        this.codTipoMadera = codTipoMadera;
+    }
+
+    public String getDescTipoMadera() {
+        return descTipoMadera;
+    }
+
+    public void setDescTipoMadera(String descTipoMadera) {
+        this.descTipoMadera = descTipoMadera;
+    }
+    
+    /**
+     * Obtener las medidas.
+     * @return las medidas
+     */
+    public String getMedidas() {
+        return medidas;
+    }
+    
+    /**
+     * Establecer medidas de producto
+     * @param medidas las medidas.
+     */
+    public void setMedidas(String medidas) {
+        this.medidas = medidas;
+    }
+    
+    /**
+     * Obtener el codigo del tipo de producto.
+     * @return codigo del tipo
+     */
+    public String getCodTipoProducto() {
+        return codTipoProducto;
+    }
+    
+    /**
+     * Establecer codigo de tipo de producto del producto
+     * @param codTipoProducto el codigo del tipo.
+     */
+    public void setCodTipoProducto(String codTipoProducto) {
+        this.codTipoProducto = codTipoProducto;
+    }
+
+    public String getDescTipoProducto() {
+        return descTipoProducto;
+    }
+
+    public void setDescTipoProducto(String descTipoProducto) {
+        this.descTipoProducto = descTipoProducto;
+    }
+    
+    /**
+     * Obtener la cantidad por unidad.
+     * @return la cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+    
+    /**
+     * Establecer cantidad de producto
+     * @param cantidad la cantidad
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    /**
+     * Obtener el precio por vara.
+     * @return el precio por vara
+     */
+    public double getPrecioXvara() {
+        return precioXvara;
+    }
+    
+    /**
+     * Establecer precio por cada vara de producto
+     * @param precioXvara El precio.
+     */
+    public void setPrecioXvara(double precioXvara) {
+        this.precioXvara = precioXvara;
+    }
+    
+    /**
+     * Obtener la descripción.
+     * @return la descripcion.
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+    
+    /**
+     * Establecer descripción de producto
+     * @param descripcion la descripción.
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    /**
+     * Obtener el estado.
+     * @return El estado
+     */
+    public Estado getEstado() {
+        return estado;
+    }
+    
+    /**
+     * Establecer estado de producto
+     * @param estado El estado.
+     */
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+    /**
+     * Obtener el codigo del proveedor.
+     * @return El codigo del proveedor
+     */
+    public String getCodProveedor() {
+        return codProveedor;
+    }
+    /**
+     * Establecer el codigo del proveedor
+     * @param codProveedor El codigo del proveedor
+     */
+    public void setCodProveedor(String codProveedor) {
+        this.codProveedor = codProveedor;
+    }
     
 }
--- src/modelos/MdlMadera.java
+++ src/modelos/MdlMadera.java
@@ -5,10 +5,92 @@
  */
 package modelos;
 
+import controladores.CtrConexion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import logica.negocio.Madera;
+
 /**
  *
  * @author aoihanabi
  */
 public class MdlMadera {
+    
+    /** Controlador de conexión. */
+    private static CtrConexion conexion;
+    /** Procedimiento a ejecutar en la base. */
+    private static String procedimiento;
+    /** Resultado de las consultas a la base. */
+    private static ResultSet resultado;
+    /** Lista de productos en la base. */
+    private static ArrayList<Madera> productos;
+    
+    /**
+     * Constructor de clase modelo de cliente.
+     */
+    public MdlMadera() {
+        conexion = new CtrConexion();
+    }
+    /**
+     * Llena una lista con todos los productos almacenados en la BD.
+     *
+     * @return lista de productos.
+     */
+    public ArrayList<Madera> obtenerProductos() {
+        productos = new ArrayList<>();
+
+        try {
+            procedimiento = "pc_obtener_productos()";
+            conexion.abrirConexion();
+            resultado = conexion.ejecutarProcedimiento(procedimiento);
+
+            String codigo;
+            String codProducto;
+            String nombre;
+            String codTipoMadera;
+            String descTipoMadera;
+            String medidas;
+            String codTipoProducto;
+            String descTipoProducto;
+            int cantidad;
+            double precioXvara;
+            String descripcion;
+            String estado;
+            String codProveedor;
+                    
+            while (resultado.next()) {
+                
+                codigo = resultado.getString("cod_Productos");
+                codProducto = resultado.getString("codProd_Productos");
+                nombre = resultado.getString("nom_Productos");
+                descripcion = resultado.getString("desc_Productos");
+                precioXvara = resultado.getDouble("precioXvara_Productos");
+                cantidad = resultado.getInt("cantidad_Productos");
+                medidas = resultado.getString("medidas_Productos");
+                codTipoMadera = resultado.getString("codTipoMadera_Productos");
+                descTipoMadera = resultado.getString("desc_TipoMadera");
+                codTipoProducto = resultado.getString("codTipoProducto_Productos");
+                descTipoProducto = resultado.getString("desc_TipoProducto");
+                codProveedor = resultado.getString("codigo_Proveedores");
+                estado = resultado.getString("estado_Productos");
+                                
+                Madera producto
+                        = new Madera(codigo, codProducto, nombre, codTipoMadera, 
+                                descTipoMadera, medidas, codTipoProducto, 
+                                descTipoProducto, cantidad, precioXvara, 
+                                descripcion, estado, codProveedor);
+
+                if (!productos.contains(producto)) {
+                    productos.add(producto);
+                }
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        } finally {
+            conexion.cerrarConexion();
+            return productos;
+        }
+    }
     
 }
